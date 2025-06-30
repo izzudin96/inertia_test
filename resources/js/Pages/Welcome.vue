5c4050413f990841a0264244600fb20f4ad2e446
Izzudin Anuar <izzudinanuar96@gmail.com>
2025-06-30 21:33:54 +1200

    fix: wip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,32 +2,31 @@
     <div class="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
         <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
             <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                    Laravel + Inertia.js + Vue 3
-                </div>
-                <h1 class="block mt-1 text-lg leading-tight font-medium text-black">
-                    Testing
-                </h1>
-                <p class="mt-2 text-gray-500">
-                    Hello World
-                </p>
-                <div class="mt-4">
-                    <p class="text-sm text-gray-600">
-                        Loaded at: {{ loadedTime }}
-                    </p>
-                    <p>
+                <form @submit.prevent="form.post('/todos')" class="space-y-4">
+                    <div class="flex flex-col">
+                        <label for="title">Title:</label>
+                        <input type="text" name="title" v-model="form.title" id="title" class="border border-gray-300 rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                        <span v-if="form.errors.title">{{ form.errors.title }}</span>
+                    </div>
 
-                    </p>
-                </div>
+                    <button class="bg-blue-400 p-2 rounded text-white hover:bg-blue-500" type="submit">
+                        Add todos
+                    </button>
+                </form>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed } from 'vue';
+import { useForm } from '@inertiajs/vue3';
 
 const loadedTime = ref('')
+
+const form = useForm({
+    title: ''
+});
 
 onMounted(() => {
     loadedTime.value = new Date().toLocaleString()
